6cd9b5b2f592026bb4da3b8fd1538b52f19b5c72
Eunho Park <ptera21c@gmail.com>
2023-12-18 22:30:45 +1100

    adding another mini project weatherapi

---
+++ weatherapi.py
@@ -0,0 +1,30 @@
+import requests 
+
+
+API_key = "4d2ebc32e94ce20f33c3e96949ef9792"
+base_rul = "https://api.openweathermap.org/data/2.5/weather"
+
+
+city = input("Enter a city: ")
+
+request_url = f"{base_rul}?q={city}&appid={API_key}&units=metric"
+response = requests.get(request_url)
+
+
+
+if response.status_code == 200:
+    data = response.json()
+   
+    main = data['weather'][0]['main']
+    #print(main)
+    temp = data['main']['temp']
+    feels_like = data['main']['feels_like']
+    temp_min = data['main']['temp_min']
+    temp_max = data['main']['temp_max']
+    print(f"temperature: {temp}'c")
+    print(f"minimum Temperature: {temp_min}'c")
+    print(f"maximum Temperature: {temp_max}'c")
+    print(f"it will feel like {feels_like}'c")
+    
+else:
+    print("error occured")
